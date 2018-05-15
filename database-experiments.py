966824aa7584bb131642185c676748db08b65ca8
Tord <tord.dellsen@gmail.com>
2018-05-15 03:19:49 +0200

    -

--- database-experiments.py
+++ database-experiments.py
@@ -14,55 +14,59 @@ print(sqlalchemy.__version__)
 db_engine = create_engine("sqlite:///sqlalchemy_test.db", echo=True)
 
 
-class Helper:
-    __session = None
-    __base_class = None
-
-    @staticmethod
-    def get_base():
-        if Helper.__base_class is None:
-            Helper.__base_class = declarative_base()
-            Helper.__base_class.metadata.create_all(db_engine)
-        return Helper.__base_class
-
-    @staticmethod
-    def get_session():
-        if Helper.__session is None:
-            # ":memory:"
-
-            SessionClass = sessionmaker()
-            SessionClass.configure(bind=db_engine)
-            Helper.__session = SessionClass()
-        return Helper.__session
+# class Helper:
+session = None
+base_class = None
 
 
-class KindPhrase(Helper.get_base()):
+def get_base():
+    global base_class
+    if base_class is None:
+        base_class = declarative_base()
+        base_class.metadata.create_all(db_engine)
+    return base_class
+
+
+def get_session():
+    global session
+    if session is None:
+        # ":memory:"
+
+        SessionClass = sessionmaker()
+        SessionClass.configure(bind=db_engine)
+        session = SessionClass()
+    return session
+
+
+class KindPhrase(get_base()):
     __tablename__ = "kind_phrases"
 
     id = Column(Integer, primary_key=True)
     phrase = Column(String)
+    # starred = Column(String, default="No")
 
     def __repr__(self):
         return "<KindPhrase(phrase='{0}')>".format(self.phrase)
+        # return "<KindPhrase(phrase='{0}', starred='{1}')>".format(self.phrase, self.starred)
 
 
 # print(KindPhrase())
 
 
-example_phrase = KindPhrase(phrase="new lines")
+example_phrase = KindPhrase(phrase="new lines again")
 print(example_phrase.phrase)
 
-Helper.get_session().add(example_phrase)
-Helper.get_session().commit()
+get_session().add(example_phrase)
+get_session().commit()
 
 count = 0
-for instance in Helper.get_session().query(KindPhrase):
+for instance in get_session().query(KindPhrase):
     if count == 1:
         instance.phrase += "edited"
     count += 1
 
-Helper.get_session().commit()
+get_session().commit()
 
-for instance in Helper.get_session().query(KindPhrase):
+for instance in get_session().query(KindPhrase):
     print(instance.phrase)
 
